ab28d001ace1b6803e4c54791b6e7ef28f2965c4
kelbert2 <kristiannaelbert@gmail.com>
2021-03-09 15:06:51 -0800

    Added random greetings and external files

---
+++ README.md
@@ -0,0 +1,19 @@
+# About DadBot
+DadBot listens for dad jokes, and then pounces! But only when you're not in a heated discussion. DadBot would hate to interrupt your 9 hour rant about how gamers are oppressed.
+
+At least you can take comfort knowing that DadBot is reading your every word, and deciding not to engage.
+
+# Run DadBot Locally
+Add a file called auth.json with 
+```JSON
+{
+    "token": "aaaa"
+}
+```
+where "aaaa" is your discord bot's authorization token.
+
+Navigate to DadBot's home (folder) and type 
+"node bot.js"
+into your command prompt.
+
+To kill him, press Ctrl+C. He'd thank you if he could.
--- bot.js
+++ bot.js
@@ -1,10 +1,9 @@
 var Discord = require('discord.js');
 var auth = require('./auth.json');
 const config = require('./config.json');
+const greetings = require('./greetings.json');
 
 var jokeTimer;
-
-// var jokeTime = false;
 
 // var bot = new Discord.Client({
 //     token: auth.token,
@@ -13,23 +12,26 @@ var jokeTimer;
 
 const bot = new Discord.Client();
 bot.on('ready', () => {
-  console.log('Stats bot is ready');
+  console.log('DadBot is ready');
 });
 
 bot.on('message', message => {
-  // jokeTime = false;
   clearTimeout(jokeTimer);
 
   if (message.author.bot) return;
 
   /* Umprompted */
-  if (message.content.match(/(I'm |I am )/i)) {
+  if (message.content.match(/(I'm |I am |I’m |I m |im )/i)) {
     // setTimeout(setJokeTime, config.inactivityTimerMs, true);
     // setImmediate(hiDadJoke, message);
     jokeTimer = setTimeout(hiDadJoke, config.inactivityTimerMs, message);
   }
 
-  if (!message.content.startsWith(config.prefix)) return;
+  if (message.content.toLowerCase().startsWith(config.altPrefix)) {
+    message.channel.send('I only respond to "daddy" ;-)');
+  }
+
+  if (!message.content.toLowerCase().startsWith(config.prefix)) return;
 
   // split around spaces
   const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
@@ -40,23 +42,23 @@ bot.on('message', message => {
       message.channel.send('pong!');
       break;
     case 'fact':
-
+      message.channel.send('capitalism is a disease and classism is a symptom.');
       break;
 
     case 'joke':
-
+      message.channel.send('Someday I\'ll have reason to say I\'m proud of you.');
       break;
 
     case 'confession':
-
+      message.channel.send('I have no idea what the NASDAQ is, only that it will be destroyed if Wal-mart has to pay their employees enough to get them off of welfare.');
       break;
 
     case 'help':
-
+      message.channel.send('I am DadBot. I respond to Dad Jokes, if I\'m not interrupted. You can say my name and then fact, joke, or confession, and I\'ll respond.');
       break;
 
     default:
-      message.channel.send('I am Dad! Please destroy this farce you call my existance.');
+      message.channel.send('I am DadBot! Please destroy this farce you call my existence.');
   }
 });
 
@@ -70,14 +72,13 @@ bot.on('disconnect', () => {
 bot.login(auth.token);
 
 function hiDadJoke(message) {
-  // if (jokeTime === false) return;
-  let name = message.content.match(/(?<=I'm |I am ).*?((?=[!?.;,])|$)/i)[0].split(/\s/).map((word, i, arr) => titleCase(word.trim()))
+  let name = message.content.match(/(?<=I'm |I am |I’m |I m |im).*?((?=[!?.;,])|$)/i)[0].split(/\s/).map((word, i, arr) => titleCase(word.trim()))
     .join(' ');
   if (name == null) {
-    message.channel.send('Hi, I\'m Dad. Just pull the trigger.');
+    message.channel.send('Hi, I\'m DadBot. Just pull the trigger.');
   }
-  message.channel.send('Hi, ' + name + ', I\'m Dad. End my suffering.');
-  // jokeTime = false;
+
+  message.channel.send(getRandomGreeting(name));
 }
 
 function titleCase(word) {
@@ -87,6 +88,8 @@ function titleCase(word) {
   return word;
 }
 
-// function setJokeTime(bool) {
-//   jokeTime = bool;
-// }
+function getRandomGreeting(name) {
+  const randIndex = Math.floor(Math.random() * greetings.greetings.length)
+  const mess = greetings.greetings[randIndex].split("[name]");
+  return mess[0] + name + mess[1];
+}
